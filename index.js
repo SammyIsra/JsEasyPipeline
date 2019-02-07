c9c931c7ad027f0c62e0afd9e30da7b14278649b
Sammy Israwi <sammy.israwi@gmail.com>
2019-02-06 22:11:17 -0500

    Uglly commit!

---
+++ index.js
@@ -0,0 +1,5 @@
+import { sayHi } from "./greetings";
+import * as Greetings from "./src/greetings";
+import * as NamesActions from "./src/names";
+
+console.log(sayHi("Sammy"));
---
+++ src/greetings.js
@@ -0,0 +1,7 @@
+export function sayHi(name = "John") {
+  return `Hi ${name}`;
+}
+
+export function sayBye(name = "John") {
+  return `Goodbye ${name}`;
+}
