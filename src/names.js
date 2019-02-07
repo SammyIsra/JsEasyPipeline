c8f162acb6c9db95acad6b78a9837bd926f01c6b
Sammy Israwi <sammy.israwi@gmail.com>
2019-02-07 00:33:01 -0500

    Initial proper commit. Welcome, all!

--- src/names.js
+++ src/names.js
@@ -1,16 +1,16 @@
 const defaultNames = [
-"Sammy",
-"David",
-"Alyssa",
-"Andrew",
-"Helen",
-"Zack",
-"Bonnie",
-"Samantha"
+  "Sammy",
+  "David",
+  "Alyssa",
+  "Andrew",
+  "Helen",
+  "Zack",
+  "Bonnie",
+  "Samantha"
 ];
 
 export function funkyFilteredNames(names = [...defaultNames]) {
-return names
-.filter(name => name.toLocaleLowerCase()[0] == "a")
-.map(name => name.toLocaleUpperCase());
+  return names
+    .filter(name => name.toLocaleLowerCase()[0] == "a")
+    .map(name => name.toLocaleUpperCase());
 }
